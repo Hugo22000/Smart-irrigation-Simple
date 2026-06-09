7c673b0409e65f5b360f96b4cf4433c5f5c33b72
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-09 17:57:16 +0200

    Refactor IrrigationButton to use number_entity

--- button.py
+++ button.py
@@ -1,22 +1,23 @@
 import asyncio
-
 from homeassistant.components.button import ButtonEntity
 
 class IrrigationButton(ButtonEntity):
 
-    def __init__(self, hass, switch_entity, duration_entity):
+    def __init__(self, hass, switch_entity, number_entity):
         self.hass = hass
-        self._switch_entity = switch_entity
-        self._duration_entity = duration_entity
+        self.switch_entity = switch_entity
+        self.number_entity = number_entity
+
+        self._attr_name = f"Arroser"
 
     async def async_press(self):
 
-        duration = self._duration_entity.native_value
+        duration = self.number_entity.native_value
 
         await self.hass.services.async_call(
             "switch",
             "turn_on",
-            {"entity_id": self._switch_entity},
+            {"entity_id": self.switch_entity},
             blocking=True,
         )
 
@@ -25,6 +26,6 @@ class IrrigationButton(ButtonEntity):
         await self.hass.services.async_call(
             "switch",
             "turn_off",
-            {"entity_id": self._switch_entity},
+            {"entity_id": self.switch_entity},
             blocking=True,
         )
