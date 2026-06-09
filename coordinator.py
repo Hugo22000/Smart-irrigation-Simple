153aa154954fbd4718efb26b8ea9b16ede11165a
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-09 17:41:51 +0200

    Update coordinator.py

--- coordinator.py
+++ coordinator.py
@@ -1,1 +1,10 @@
+class IrrigationCoordinator:
+    def __init__(self, hass, config):
+        self.hass = hass
+        self.config = config
 
+        self.durations = {
+            1: 30,
+            2: 30,
+            3: 30,
+        }
