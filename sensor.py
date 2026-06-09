a9e81e292632aaa8231c340b2e0314273a325789
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-09 17:56:35 +0200

    Refactor IrrigationVolumeSensor constructor and logic

--- sensor.py
+++ sensor.py
@@ -2,15 +2,16 @@ from homeassistant.components.sensor import SensorEntity
 
 class IrrigationVolumeSensor(SensorEntity):
 
-    def __init__(self, name, duration_entity, flow_rate):
-        self._name = f"{name} volume"
-        self._duration_entity = duration_entity
-        self._flow_rate = flow_rate
+    def __init__(self, pump_id, number_entity, flow_rate):
+        self.pump_id = pump_id
+        self.number_entity = number_entity
+        self.flow_rate = flow_rate
+        self._attr_name = f"Pompe {pump_id} volume"
 
     @property
     def native_value(self):
-        duration = self._duration_entity.native_value
-        return round(self._flow_rate * duration / 60)
+        duration = self.number_entity.native_value
+        return round(self.flow_rate * duration / 60)
 
     @property
     def native_unit_of_measurement(self):
