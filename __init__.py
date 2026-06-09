69cbd454d7b9b958f721c3a84bef21b68e7b1af6
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-09 17:52:12 +0200

    Improve entry management in async setup and unload
    Refactor async_setup_entry and async_unload_entry to manage entry data.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
-DOMAIN = "smart_irrigation_simple"
+from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     return True
 
 async def async_unload_entry(hass, entry):
+    hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
