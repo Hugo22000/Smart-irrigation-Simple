bcbeb012cd73745ff96d252bb330a8f5fbfdc83a
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-10 07:37:44 +0200

    Refactor setup and unload functions for integration

--- custom_components/Smart_irrigation_Simple/ __init__.py
+++ custom_components/Smart_irrigation_Simple/ __init__.py
@@ -1,10 +1,29 @@
-from .const import DOMAIN
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-async def async_setup_entry(hass, entry):
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+):
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
     return True
 
-async def async_unload_entry(hass, entry):
-    hass.data[DOMAIN].pop(entry.entry_id, None)
-    return True
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+):
+    return await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
