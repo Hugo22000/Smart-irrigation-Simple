130806c4f7931af5ed4b04849abad48f26d45502
Hugo22000 <92339625+Hugo22000@users.noreply.github.com>
2026-06-13 14:56:29 +0200

    Add missing constants for config flow keys and PLATFORMS

--- custom_components/Smart_irrigation_Simple/const.py
+++ custom_components/Smart_irrigation_Simple/const.py
@@ -10,3 +10,13 @@ CONF_PUMPS = "pumps"
 CONF_NAME = "name"
 CONF_SWITCH = "switch"
 CONF_FLOW = "flow"
+
+# Config flow keys
+SIS_PUMP1_SWITCH = "SIS_pump1_switch"
+SIS_PUMP1_FLOW = "SIS_pump1_flow"
+SIS_PUMP2_SWITCH = "SIS_pump2_switch"
+SIS_PUMP2_FLOW = "SIS_pump2_flow"
+SIS_PUMP3_SWITCH = "SIS_pump3_switch"
+SIS_PUMP3_FLOW = "SIS_pump3_flow"
+
+PLATFORMS = ["sensor", "button", "number"]
